c558e24b80695dddcc7536f5f3848cddbb5f2238
RAMAROSON <francis.ramarosonj@gmail.com>
2023-12-05 18:46:16 +0100

    amélioration des 3 scripts

--- extract_products_categorys_csv_img.py
+++ extract_products_categorys_csv_img.py
@@ -16,7 +16,7 @@ def download_image(url, folder_path, title):
         for chunk in response.iter_content(chunk_size=128):
             file.write(chunk)
     print(f"Image téléchargée et enregistrée: {filename}")
-
+    
 # Fonction pour obtenir un nom de fichier valide
 def get_valid_filename(title):
     clean_title = re.sub(r'[^a-zA-Z0-9 ]', '', title)
@@ -24,12 +24,15 @@ def get_valid_filename(title):
 
 # Base URL du site
 base_url = 'http://books.toscrape.com/'
-output_folder = 'download + Images'
-csv_folder = 'download+CSV'
+output_folder = 'download/Images'
+csv_folder = 'download/CSV'
 
 # Créer un répertoire pour les fichiers CSV
 os.makedirs(csv_folder, exist_ok=True)
 
+def extract_review_rating(review_rating_class):
+    rating_mapping = {'One': 1, 'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5}
+    return rating_mapping.get(review_rating_class, None)
 
 # Fonction pour extraire les informations produit d'une page produit
 def extract_product_info(product_url, category):
@@ -42,12 +45,12 @@ def extract_product_info(product_url, category):
         title = soup.find('h1').text
         price_including_tax = soup.find('th', string='Price (incl. tax)').find_next('td').text.replace('Â','')
         price_excluding_tax = soup.find('th', string='Price (excl. tax)').find_next('td').text.replace('Â','')
-        number_available = soup.find('th', string='Availability').find_next('td').text
+        number_available_text = soup.find('th', string='Availability').find_next('td').text
+        number_available = int(re.search(r'\b(\d+)\b', number_available_text).group())
         product_description = soup.find('meta', {'name': 'description'})['content']
-        review_rating = soup.find('p', {'class': 'star-rating'})['class'][1]
+        review_rating_class = soup.find('p', {'class': 'star-rating'})['class'][1]
+        review_rating = extract_review_rating(review_rating_class)
         image_url = base_url + soup.find('img')['src']
-
-       
         # Créez un DataFrame avec les informations sur le produit
         df = pd.DataFrame({
             'product_page_url': [product_page_url],
@@ -61,31 +64,27 @@ def extract_product_info(product_url, category):
             'review_rating': [review_rating],
             'image_url': [image_url]
         })
-
         # Écrivez le DataFrame dans un fichier CSV dans le répertoire "CSV"
         csv_filename = os.path.join(csv_folder, f'{category}.csv')
         df.to_csv(csv_filename, index=False, mode='a', header=not os.path.exists(csv_filename), encoding='utf-8')
-
         # Téléchargez et enregistrez l'image dans le répertoire des catégories
         download_image(image_url, os.path.join(output_folder, category), title)
     else:
-        print(f'Erreur {response.status_code} pendan la requête{product_url}')
-
+        print(f'Erreur {response.status_code} pendant la requête{product_url}')
+        
 # Fonction pour extraire les URLs de tous les produits dans une catégorie
 def extract_all_product_urls(category_url):
     all_urls = []
     while category_url:
         response = requests.get(category_url)
         soup = BeautifulSoup(response.text, 'html.parser')
-        
         # Trouver toutes les balises <h3> qui contiennent les liens vers les pages de produits
         product_links = soup.select('h3 a')
-        
         # Construire la liste complète des URLs des produits
         base_url = 'https://books.toscrape.com/catalogue/'
         product_urls = [base_url + link['href'][9:] for link in product_links]
         all_urls.extend(product_urls)
-
+        
         # Extraire l'URL de la page suivante
         next_page = soup.find('li', class_='next')
         if next_page:
@@ -94,13 +93,11 @@ def extract_all_product_urls(category_url):
             break
     
     return all_urls
-
 # Obtenir la page d'accueil
 response = requests.get(base_url)
 
 if response.status_code == 200:
     soup = BeautifulSoup(response.text, 'html.parser')
-    
     # Trouver toutes les catégories
     categories = soup.select('div.side_categories ul.nav-list li ul li a')
     
@@ -108,11 +105,9 @@ if response.status_code == 200:
         category_url = base_url + category['href']
         category_name = category.text.strip()
         print(f'Exploration de la catégorie: {category_name}...')
-        
-        # Créer un répertoire pour la catégorie
+         # Créer un répertoire pour la catégorie
         os.makedirs(os.path.join(output_folder, category_name), exist_ok=True)
-
-        # Extraire les données de tous les produits de la catégorie
+         # Extraire les données de tous les produits de la catégorie
         product_urls = extract_all_product_urls(category_url)
 
         for product_url in product_urls:
